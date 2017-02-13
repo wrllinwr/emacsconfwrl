4e4d18e1229a0d9fe06b2db14d61d7603aff8621
wrllinwr <lin_weiran@163.com>
2017-02-13 11:30:41 +0800

    update init.el

--- init.el
+++ init.el
@@ -13,6 +13,9 @@
  '(custom-safe-themes
    (quote
     ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+ '(evil-leader/leader "SPC")
+ '(evil-want-C-u-scroll t)
+ '(popwin:popup-window-position (quote bottom))
  '(speedbar-show-unknown-files t)
  '(sr-speedbar-right-side nil))
 (custom-set-faces
@@ -20,4 +23,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(iedit-occurrence ((t (:inherit region)))))
