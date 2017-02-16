2049a0871dae634b07b6a0ff3704ffc960fb77cd
wrllinwr <lin_weiran@163.com>
2017-02-16 19:25:19 +0800

    remove emacs24-starter-kit-wrl dire

--- emacs24-starter-kit-wrl/elpa/nodejs-repl-20170110.940/nodejs-repl-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "nodejs-repl" "20170110.940" "Run Node.js REPL" 'nil)
--- emacs24-starter-kit-wrl/lisp/init-ui.el
+++
@@ -1,8 +0,0 @@
-(global-linum-mode t)
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq inhibit-splash-screen t)
-
-(provide 'init-ui)
